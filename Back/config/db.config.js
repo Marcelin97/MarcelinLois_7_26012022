b6213cdea8267b980f594b4664b55dfb0e4cc5b2
Marcelin97 <lois_m@outlook.com>
2022-02-04 09:30:15 +0100

    add logger bunyan and configuration

--- Back/config/db.config.js
+++ Back/config/db.config.js
@@ -1,5 +1,30 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
+const bunyan = require("bunyan");
+
+const log = bunyan.createLogger({
+  name: "MySQL Driver",
+  streams: [
+    {
+      stream: process.stdout,
+      level: "info"
+    },
+    {
+      stream: process.stdout,
+      level: "debug"
+    },
+    {
+      stream: process.stderr,
+      level: "error"
+    },
+    {
+      type: "rotating-file",
+      path: "./logs/MySQL.log",
+      period: "1d", // daily rotation
+      count: 3 // keep 3 back copies
+    }
+  ]
+});
 
 let host = process.env.MYSQL_HOST || "localhost";
 let database = process.env.MYSQL_DATABASE || "groupomania";
@@ -8,18 +33,19 @@ let password = process.env.MYSQL_PASSWORD;
 let port = process.env.MYSQL_PORT || 3306;
 
 const sequelize = new Sequelize(database, username, password, {
-    host: host,
-    port: port,
+  host: host,
+  port: port,
   dialect: "mysql",
+  logging: log.debug.bind(log) // Alternative way to use custom logger, displays all messages
 });
 
 try {
-    (async() => {
-        await sequelize.authenticate();
-    })().catch(err => {
-       console.log(err)
-    })
-    console.log('connection has established')
+  (async () => {
+    await sequelize.authenticate();
+  })().catch(err => {
+    console.log(err);
+  });
+  console.log("connection has established");
 } catch (error) {
-        console.log('Unable to connect', error)
+  console.log("Unable to connect", error);
 }
