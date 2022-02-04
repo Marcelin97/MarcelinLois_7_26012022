41628eccddd1466a5c06e52b3da5aeb80b6ab1bd
Marcelin97 <lois_m@outlook.com>
2022-02-04 01:29:45 +0100

    test connection to my database

--- Back/config/db.config.js
+++ Back/config/db.config.js
@@ -1,20 +1,25 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let host = process.env.MYSQL_HOST || "localhost";
 let database = process.env.MYSQL_DATABASE || "groupomania";
 let username = process.env.MYSQL_USERNAME || "root";
-let password = process.env.MYSQL_PASSWORD || "";
+let password = process.env.MYSQL_PASSWORD;
 let port = process.env.MYSQL_PORT || 3306;
 
 const sequelize = new Sequelize(database, username, password, {
-  host: host,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+    host: host,
+    port: port,
+  dialect: "mysql",
 });
+
+try {
+    (async() => {
+        await sequelize.authenticate();
+    })().catch(err => {
+       console.log(err)
+    })
+    console.log('connection has established')
+} catch (error) {
+        console.log('Unable to connect', error)
+}
