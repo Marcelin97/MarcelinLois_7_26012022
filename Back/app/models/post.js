05b060add39cdf3749f436591016220cb4be5cdd
Marcelin97 <lois_m@outlook.com>
2022-02-07 00:06:16 +0100

    create post model - add imageUrl

--- Back/app/models/post.js
+++ Back/app/models/post.js
@@ -1,59 +1,55 @@
 module.exports = (sequelize, Sequelize) => {
   const Post = sequelize.define("user", {
-
     title: {
       type: Sequelize.STRING,
       unique: false,
       allowNull: false,
       validate: {
-        len: [5, 255],
-      },
-      },
-      createdAt: {
-            type: Date,
-            default: Date.now()
-      },
-      createdBy: {
-            type: Sequelize.STRING,
-            ref: "User",
-            required: true
-          },
+        len: [5, 255]
+      }
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    createdBy: {
+      type: Sequelize.STRING,
+      ref: "User",
+      required: true
+    },
+    imageUrl: {
+      type: Sequelize.STRING,
+      required: false,
+      default: ""
+    },
     content: {
       type: Sequelize.TEXT,
-        unique: false,
-        required: false,
+      unique: false,
+      required: false,
       default: "",
       allowNull: false,
       validate: {
-        len: [20, 5000],
-      },
+        len: [20, 5000]
+      }
     },
     upVotes: {
       type: Sequelize.INTEGER,
       unique: false,
-      allowNull: false,
-    //   defaultValue: 0,
-      },
-    upVotes: {
-      type: Sequelize.INTEGER,
-      unique: false,
-      allowNull: false,
-    //   defaultValue: 0,
-      },
+      allowNull: false
+      //   defaultValue: 0,
+    },
     downVotes: {
       type: Sequelize.INTEGER,
       unique: false,
-      allowNull: false,
-    //   defaultValue: 0,
+      allowNull: false
+      //   defaultValue: 0,
     },
     comments: {
       type: Sequelize.INTEGER,
       unique: false,
       allowNull: false,
-      defaultValue: 0,
-    },
-      
-      
+      defaultValue: 0
+    }
   });
   return Post;
 };
