d6ac4e7180ae7c7614229e4d7cba07ec52f98615
Marcelin97 <lois_m@outlook.com>
2022-04-07 00:20:26 +0200

    install sass and start home page

---
+++ front/groupomania/src/components/Loader.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="Clouds">
+
+    <!-- <img src="https://assets.codepen.io/1538474/astronaut.svg" class="logo" /> -->
+ <img src="../assets/img/logo.svg" class="logo" alt="">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'LoaderHome'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#Clouds {
+  position: absolute;
+  margin: auto;
+  user-select: none;
+}
+
+.logo{
+  height: 10vmin;
+    animation: floating 3s infinite ease-in-out;
+}
+
+@keyframes floating {
+    from { transform: translateY(0px); }
+    65%  { transform: translateY(15px); }
+    to   { transform: translateY(-0px); }
+}
+</style>
--- front/groupomania/src/views/HomeView.vue
+++ front/groupomania/src/views/HomeView.vue
@@ -1,19 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <font-awesome-icon :icon="['fas', 'user-secret']"  /> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <div class="load">
+      <LoaderHome />
+    </div>
+    <div class="home">
+      <!-- <font-awesome-icon :icon="['fas', 'user-secret']"  /> -->
+      <HelloWorld msg="Bienvenue sur Groupomania" />
+    </div>
+    <nav>
+      <router-link class="nav" to="/about">Découvrir</router-link>
+      <router-view />
+    </nav>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import LoaderHome from "@/components/Loader.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    LoaderHome,
+  },
+};
 </script>
+
+<style scoped lang="scss">
+.load {
+  margin-bottom: 5em;
+}
+nav{ 
+  display:flex;
+  justify-content:flex-end;
+}
+.nav {
+  display:flex;
+align-items:center;
+    padding: 0 1.5rem;
+    border-radius: 2rem;
+    font-size: 1rem;
+    color: #0c131f;
+    min-height: 3rem;
+    background-color: #8de8fe;
+    transition: background .25s;
+    &:hover{
+      background-color: #62c5de;
+    }
+}
+</style>
