9db9f14ff755b8fadf8be233d01e8071f371e89c
Marcelin97 <lois_m@outlook.com>
2022-07-25 09:53:53 +0200

    review structure of my code in UserView and ProfileView

--- front/src/components/Profil/UserProfile.vue
+++ front/src/components/Profil/UserProfile.vue
@@ -57,7 +57,7 @@
             Signaler...
           </button>
         </div>
-        
+
         <div v-else>
           <!-- update profile -->
           <router-link class="btn" to="/user/parameter">
@@ -151,113 +151,183 @@
 
 </template>
 <script>
-// import PostCard from "../Posts/PostCard.vue";
-// import modalStructure from "../Modal/ModalStructure.vue";
-// import deleteBtn from "../Base/DeleteBtn.vue";
-// import usersApi from "../../api/users";
-// import axiosInstance from "../../services/api";
-// import userApi from "../../api/users";
-// import useVuelidate from "@vuelidate/core";
-// import {
-//   helpers,
-//   // required,
-//   minLength,
-//   maxLength,
-// } from "@vuelidate/validators";
-// import { reactive, computed } from "vue";
+import PostCard from "../Posts/PostCard.vue";
+import modalStructure from "../Modal/ModalStructure.vue";
+import deleteBtn from "../Base/DeleteBtn.vue";
+import usersApi from "../../api/users";
+import axiosInstance from "../../services/api";
+import useVuelidate from "@vuelidate/core";
+import {
+  helpers,
+  // required,
+  minLength,
+  maxLength,
+} from "@vuelidate/validators";
+import { reactive, computed } from "vue";
 
 export default {
-  props: ["user", "userLoggedIn", "targetUser"]
+  name: "User-profile",
+  props: ["user", "userLoggedIn", "targetUser"],
+  setup() {
+    const state = reactive({
+      user: {
+        content: "",
+      },
+    });
+
+    const rules = computed(() => ({
+      user: {
+        content: {
+          // required: helpers.withMessage("L'/email est obligatoire", required),
+          $autoDirty: true,
+          $lazy: true,
+          minLength: helpers.withMessage(
+            "Ce champ doit être long d'au moins 5",
+            minLength(5)
+          ),
+          maxLength: helpers.withMessage(
+            "La longueur maximale autorisée est de 255",
+            maxLength(255)
+          ),
+        },
+      },
+    }));
+
+    const v$ = useVuelidate(rules, state);
+
+    return { state, v$ };
+  },
+  validationConfig: {
+    $lazy: true,
+  },
+  components: {
+    PostCard,
+    modalStructure,
+    deleteBtn,
+  },
+  data() {
+    return {
+      apiError: "",
+    };
+  },
+  methods: {
+    async exportDataClick() {
+      try {
+        const response = await usersApi.exportMyData();
+        var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+        var fileLink = document.createElement("a");
+
+        fileLink.href = fileURL;
+        fileLink.setAttribute("download", "file.txt");
+        document.body.appendChild(fileLink);
+
+        fileLink.click();
+
+        this.$notify({
+          type: "success",
+          text: "Le téléchargement commence.",
+        });
+      } catch (error) {
+        const errorMessage = (this.apiError = error.response);
+        this.errorMessage = errorMessage;
+        // console.log("apiError", error);
+
+        // notification d'erreur
+        this.$notify({
+          duration: 2500,
+          type: "error",
+          title: `Erreur lors du téléchargement des données`,
+          text: `Erreur reporté : ${errorMessage}`,
+        });
+      }
+    },
+    async deleteAccountClick() {
+      if (
+        window.confirm(
+          "Attention, vous êtes sur le point de supprimer votre compte. Cette action est irréversible. Souhaitez-vous tout de même continuer ?"
+        )
+      ) {
+        try {
+          await usersApi.deleteUser();
+          await this.$store.commit("logout");
+          // await this.$store.commit("setStatus", "logout");
+          await this.$router.push("/");
+        } catch (e) {
+          console.error(e.data);
+        }
+      }
+    },
+    reportAccountClick() {
+      this.v$.$validate(); // checks all inputs
+      if (!this.v$.$error) {
+        // if ANY fail validation
+        axiosInstance
+          .post(`/auth/report/${this.userId}`, this.state.user)
+          .then(() => {
+            // notification de succès
+            this.$notify({
+              type: "success",
+              title: `Signalement envoyé !`,
+              text: `Vous allez être redirigé vers votre profil.`,
+            });
+
+            // force refresh page
+            setTimeout(
+              function () {
+                this.$router.go(0);
+              }.bind(this),
+              1000,
+              this
+            );
+          })
+          .catch((error) => {
+            console.log(error.response.status);
+            if (error.response.status == 404) {
+              const errorMessage = (this.apiError =
+                "Utilisateur introuvable !");
+              this.errorMessage = errorMessage;
+              // notification d'erreur
+              this.$notify({
+                type: "error",
+                title: `Erreur lors du signalement`,
+                text: `Erreur reporté : ${errorMessage}`,
+              });
+            } else if (error.response.status == 409) {
+              const errorMessage = (this.apiError =
+                "Vous avez déjà signalé cet utilisateur !");
+              this.errorMessage = errorMessage;
+              // notification d'erreur
+              this.$notify({
+                type: "error",
+                title: `Erreur lors du signalement`,
+                text: `Erreur reporté : ${errorMessage}`,
+              });
+            }
+          });
+      } else {
+        // notification d'erreur
+        this.$notify({
+          type: "warn",
+          title: `Veuillez faire un signalement complet.`,
+        });
+
+        // montre les erreurs à l'écran
+        this.$nextTick(() => {
+          let domRect = document
+            .querySelector(".error")
+            .getBoundingClientRect();
+          window.scrollTo(
+            domRect.left + document.documentElement.scrollLeft,
+            domRect.top + document.documentElement.scrollTop
+          );
+        });
+      }
+    },
+  },
 };
 </script>
 
-
 <style lang="scss" scoped>
-.ribbon {
-  position: absolute;
-  right: -5px;
-  top: -5px;
-  z-index: 1;
-  overflow: hidden;
-  width: 75px;
-  height: 75px;
-  text-align: right;
-}
-
-.ribbon span {
-  font-size: 10px;
-  font-weight: bold;
-  color: #fff;
-  text-transform: uppercase;
-  text-align: center;
-  line-height: 20px;
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-  width: 100px;
-  display: block;
-  background: #79a70a;
-  background: linear-gradient(#9bc90d 0%, #79a70a 100%);
-  box-shadow: 0 3px 10px -5px rgba(0, 0, 0, 1);
-  position: absolute;
-  top: 19px;
-  right: -21px;
-}
-
-.ribbon span::before {
-  content: "";
-  position: absolute;
-  left: 0px;
-  top: 100%;
-  z-index: -1;
-  border-left: 3px solid #79a70a;
-  border-right: 3px solid transparent;
-  border-bottom: 3px solid transparent;
-  border-top: 3px solid #79a70a;
-}
-
-.ribbon span::after {
-  content: "";
-  position: absolute;
-  right: 0px;
-  top: 100%;
-  z-index: -1;
-  border-left: 3px solid transparent;
-  border-right: 3px solid #79a70a;
-  border-bottom: 3px solid transparent;
-  border-top: 3px solid #79a70a;
-}
-
-.pro {
-  /* Shown at the bottom right corner */
-  top: 0;
-  position: absolute;
-  right: 0;
-  transform: translate(50%, 50%);
-
-  /* Rounded border */
-  border-radius: 0 20px 20px 0;
-  width: auto;
-
-  /* Background color */
-  color: #231e39;
-  background-color: #febb0b;
-  font-size: 14px;
-  font-weight: bold;
-  padding: 3px 7px;
-}
-
-// modalStructure
-.overflow-hidden {
-  overflow: hidden;
-}
-
-.modal__actions {
-  padding: 2rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
-
 // Profil
 .wrapper {
   width: 100%;
@@ -409,5 +479,60 @@ img {
     outline: none;
     border-color: #b44ff6;
   }
+}
+
+// Ribbon
+.ribbon {
+  position: absolute;
+  right: -5px;
+  top: -5px;
+  z-index: 1;
+  overflow: hidden;
+  width: 75px;
+  height: 75px;
+  text-align: right;
+}
+
+.ribbon span {
+  font-size: 10px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  line-height: 20px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  width: 100px;
+  display: block;
+  background: #79a70a;
+  background: linear-gradient(#9bc90d 0%, #79a70a 100%);
+  box-shadow: 0 3px 10px -5px rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 19px;
+  right: -21px;
+}
+
+.ribbon span::before {
+  content: "";
+  position: absolute;
+  left: 0px;
+  top: 100%;
+  z-index: -1;
+  border-left: 3px solid #79a70a;
+  border-right: 3px solid transparent;
+  border-bottom: 3px solid transparent;
+  border-top: 3px solid #79a70a;
+}
+
+.ribbon span::after {
+  content: "";
+  position: absolute;
+  right: 0px;
+  top: 100%;
+  z-index: -1;
+  border-left: 3px solid transparent;
+  border-right: 3px solid #79a70a;
+  border-bottom: 3px solid transparent;
+  border-top: 3px solid #79a70a;
 }
 </style>
