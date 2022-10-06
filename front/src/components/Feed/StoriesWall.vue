06509144484fda4fd8cffe78d607fe1a9cd0734c
Marcelin97 <lois_m@outlook.com>
2022-10-06 19:26:17 +0200

    review storyCard in the wall with las communities add

--- front/src/components/Feed/StoriesWall.vue
+++ front/src/components/Feed/StoriesWall.vue
@@ -1,15 +1,57 @@
 <template>
   <section>
-    <StoryCard />
+    <StoryCard
+      v-for="(community, index) in filteredCommunities"
+      :key="index"
+      :community="community"
+    />
   </section>
 </template>
 <script>
 import StoryCard from "./StoryCard.vue";
+import axiosInstance from "../../services/api";
 
 export default {
   name: "Stories-Wall",
   components: {
     StoryCard,
+  },
+  data() {
+    return {
+      // add communities array:
+      communities: [],
+      apiError: "",
+      search: "",
+    };
+  },
+  computed: {
+    filteredCommunities() {
+      return this.communities.filter((community) =>
+        community.title.toLowerCase().includes(this.search.toLowerCase())
+      );
+    },
+  },
+  mounted() {
+    axiosInstance
+      .get("/community/readAllCommunities")
+      .then((response) => {
+        this.communities = response.data.datas;
+      })
+      .catch((error) => {
+        // console.log(error.response.status);
+        if (error.response.status == 404) {
+          const errorMessage = (this.apiError =
+            "Il n'y pas encore de communauté(s) !");
+          this.errorMessage = errorMessage;
+
+          // notification d'erreur
+          this.$notify({
+            type: "info",
+            title: `Information de l'api`,
+            text: `${errorMessage}`,
+          });
+        }
+      });
   },
 };
 </script>
--- front/src/components/Feed/StoryCard.vue
+++ front/src/components/Feed/StoryCard.vue
@@ -4,16 +4,14 @@
       <div class="left-col">
         <div class="status-wrapper">
           <div class="status-card">
-            <div class="profile-pic"><img src="img/cover 1.png" alt="" /></div>
-            <p class="username">user_name_1</p>
-          </div>
-          <div class="status-card">
-            <div class="profile-pic"><img src="img/cover 2.png" alt="" /></div>
-            <p class="username">user_name_2</p>
-          </div>
-          <div class="status-card">
-            <div class="profile-pic"><img src="img/cover 3.png" alt="" /></div>
-            <p class="username">user_name_3</p>
+            <div class="profile-pic">
+              <img
+                :src="`http://localhost:3000${community.icon}`"
+                :alt="'Avatar de ' + community.title"
+                aria-label="Photo de la communauté"
+              />
+            </div>
+            <p class="username">{{ community.title }}</p>
           </div>
         </div>
       </div>
@@ -21,79 +19,95 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "Community-List",
+  props: ["community"],
+  data() {
+    return {
+      id: "",
+    };
+  },
+  mounted() {
+    this.id = this.community.id;
+    // console.log(this.id);
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-.main{
-    width: 100%;
-    padding: 40px 0;
-    display: flex;
-    justify-content: center;
-    margin-top: 50px;
+.main {
+  width: 100%;
+  padding: 40px 0;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
 }
 
-.wrapper{
-    width: 70%;
-    max-width: 1000px;
-    display: grid;
-    grid-template-columns: 60% 40%;
-    grid-gap: 30px;
+.wrapper {
+  width: 70%;
+  max-width: 1000px;
+  display: grid;
+  grid-template-columns: 60% 40%;
+  grid-gap: 30px;
 }
 
-.left-col{
-    display: flex;
-    flex-direction: column;
+.left-col {
+  display: flex;
+  flex-direction: column;
 }
 
-.status-wrapper{
-    width: 100%;
-    height: 120px;
-    background: #fff;
-    border: 1px solid #dfdfdf;
-    border-radius: 2px;
-    padding: 10px;
-    padding-right: 0;
-    display: flex;
-    align-items: center;
-    overflow: hidden;
-    overflow-x: auto;
+.status-wrapper {
+  width: 100%;
+  height: 120px;
+  background: #fff;
+  border: 1px solid #dfdfdf;
+  border-radius: 2px;
+  padding: 10px;
+  padding-right: 0;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  overflow-x: auto;
 }
 
-.status-wrapper::-webkit-scrollbar{
-    display: none;
+.status-wrapper::-webkit-scrollbar {
+  display: none;
 }
 
-.status-card{
-    flex: 0 0 auto;
-    width: 80px;
-    max-width: 80px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-right: 15px;
+.status-card {
+  flex: 0 0 auto;
+  width: 80px;
+  max-width: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 15px;
 }
 
-.profile-pic{
-    width: 70px;
-    height: 70px;
-    border-radius: 50%;
-    overflow: hidden;
-    padding: 3px;
-    background: linear-gradient(45deg, #ffd7d7, #f34642  80%);
+.profile-pic {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  overflow: hidden;
+  padding: 3px;
+  background: linear-gradient(45deg, #ffd7d7, #f34642 80%);
 }
 
-.profile-pic img{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 50%;
-    border: 2px solid #fff;
+.profile-pic img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 2px solid #fff;
 }
 
-.username{
-    width: 100%;
-    overflow: hidden;
-    text-align: center;
-    font-size: 12px;
-    margin-top:5px;
-    color: rgba(0, 0, 0, 0.5)
+.username {
+  width: 100%;
+  overflow: hidden;
+  text-align: center;
+  font-size: 12px;
+  margin-top: 5px;
+  color: rgba(0, 0, 0, 0.5);
 }
 </style>
